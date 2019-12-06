6737da2930017f5d6abd293ceeabe59649ef0fd6
Aanish97 <l164144@lhr.nu.edu.pk>
2019-12-06 10:55:00 +0500

    commit

--- holtsTrend.py
+++ holtsTrend.py
@@ -1,1 +1,21 @@
-print("holts trend")
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
+
+
+df = pd.read_excel("after_ETL.xlsx", encoding="ISO-8859-1", sep=';')
+
+df['InvoiceDate'] = pd.to_datetime(df.InvoiceDate, errors='coerce')
+df['InvoiceDate'] = df['InvoiceDate'].dt.strftime('%Y%m')
+
+
+df = df.groupby(['InvoiceDate'])['UnitPrice'].sum()
+df = df.to_frame()
+
+df.to_excel('holtsTrend.xlsx')
+df = pd.read_excel("holtsTrend.xlsx", encoding="ISO-8859-1", sep=';')
+
+#plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.plot(df["InvoiceDate"], df["UnitPrice"])
+plt.show()
--- preProcessing.py
+++ preProcessing.py
@@ -2,17 +2,24 @@ import pandas as pd
 
 #reading csv into dataframe
 #"D:/.Semester 7/FYP/FYP docx/data.csv"
-df = pd.read_csv("data.csv", encoding="ISO-8859-1")
+df = pd.read_csv("D:/.Semester 7/FYP/FYP docx/data.csv", encoding="ISO-8859-1")
 
 #de_duplication
 df.drop_duplicates(subset=['InvoiceNo', 'Description', 'Quantity', 'InvoiceDate', 'CustomerID', 'UnitPrice', 'Country'], keep='first', inplace=True)
 
 #date_format_standardization
 df['InvoiceDate'] = pd.to_datetime(df.InvoiceDate, errors='coerce')
-df['InvoiceDate'] = df['InvoiceDate'].dt.strftime('%e %m,%Y')
+df['InvoiceDate'] = df['InvoiceDate'].dt.strftime('%e -%m -%Y')
 
 
-df.to_excel('after_ETL.xlsx')
+#transaction wise data
+new_df = df.groupby(['InvoiceNo', 'InvoiceDate', 'CustomerID', 'Country'])['UnitPrice'].sum()
+new_dff = new_df.to_frame()
+
+#export_csv = new_dff.to_csv (r'D:\.Semester 7\FYP\FYP docx\data_updated.csv', header=True)
+#new_dff
+
+new_dff.to_excel('after_ETL.xlsx')
 
 print(df.head)
 
