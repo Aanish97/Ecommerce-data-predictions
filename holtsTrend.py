9c2b3a213cb62f5a26311378a55d067ed17e4f2b
Aanish97 <l164144@lhr.nu.edu.pk>
2019-12-06 13:51:59 +0500

    commit

--- holtsTrend.py
+++ holtsTrend.py
@@ -27,49 +27,30 @@ df_test=df[start_count+1:]
 #print(df_train["UnitPrice"])
 #print(df_train["InvoiceDate"])
 index= pd.date_range(start='01-2010', end='12-2011', freq='M')
-data = pd.Series(df_train["UnitPrice"].values, df_train["InvoiceDate"])
-print(data)
+data_train = pd.Series(df_train["UnitPrice"].values, df_train["InvoiceDate"])
+data_test = pd.Series(df_test["UnitPrice"].values, df_test["InvoiceDate"])
+print(data_train)
 
-#ax=ans.plot()
-#ax.set_xlabel("Date")
-#ax.set_ylabel("Unit Price")
-#plt.show()
-
-
-#exponential linear trend
-
-#fit1 = SimpleExpSmoothing(data).fit(smoothing_level=0.2,optimized=False)
-#fcast1 = fit1.forecast(3).rename(r'$\alpha=0.2$')
-#fit2 = SimpleExpSmoothing(data).fit(smoothing_level=0.6,optimized=False)
-#fcast2 = fit2.forecast(3).rename(r'$\alpha=0.6$')
-#fit3 = SimpleExpSmoothing(data).fit()
-#fcast3 = fit3.forecast(3).rename(r'$\alpha=%s$'%fit3.model.params['smoothing_level'])
-
-#ax = data.plot(marker='o', color='black', figsize=(12,8))
-#fcast1.plot(marker='o', ax=ax, color='blue', legend=True)
-#fit1.fittedvalues.plot(marker='o', ax=ax, color='blue')
-#fcast2.plot(marker='o', ax=ax, color='red', legend=True)
-
-#fit2.fittedvalues.plot(marker='o', ax=ax, color='red')
-#fcast3.plot(marker='o', ax=ax, color='green', legend=True)
-#fit3.fittedvalues.plot(marker='o', ax=ax, color='green')
-#plt.show()
 
 
 #holts linear trend
-fit1 = Holt(data).fit(smoothing_level=0.8, smoothing_slope=0.2, optimized=False)
-fcast1 = fit1.forecast(5).rename("Holt's linear trend")
-
+fit1 = Holt(data_train).fit(smoothing_level=0.6, smoothing_slope=0.4, optimized=False)
+fcast1 = fit1.forecast(30).rename("Holt's linear trend")
 #fit2 = Holt(data, exponential=True).fit(smoothing_level=0.8, smoothing_slope=0.2, optimized=False)
 #fcast2 = fit2.forecast(5).rename("Exponential trend")
-#fit3 = Holt(data, damped=True).fit(smoothing_level=0.8, smoothing_slope=0.2)
-#fcast3 = fit3.forecast(5).rename("Additive damped trend")
+#additive damped trend
+#fit3 = Holt(data_train, damped=True).fit(smoothing_level=0.8, smoothing_slope=0.2)
+#fcast3 = fit3.forecast(50).rename("Additive damped trend")
 
-ax = data.plot(color="black", marker="o", figsize=(12,8))
+
+ax = df.plot(color="black", marker="o", figsize=(12,8))
+print("---------------------")
+print(fit1.fittedvalues)    #holts trend values for training data
+
+#ax = data_test.plot(color="yellow", marker="o", figsize=(12,8))
 
 fit1.fittedvalues.plot(ax=ax, color='blue')
 fcast1.plot(ax=ax, color='blue', marker="o", legend=True)
-
 #fit2.fittedvalues.plot(ax=ax, color='red')
 #fcast2.plot(ax=ax, color='red', marker="o", legend=True)
 #fit3.fittedvalues.plot(ax=ax, color='green')
